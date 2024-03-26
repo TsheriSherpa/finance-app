23b43144325c957e19edd5eb0ce501e303424678
chiree sherpa <tsherisherpa@gmail.com>
2024-03-26 17:29:45 -0400

    added stack navigator

--- App.js
+++ App.js
@@ -1,22 +1,52 @@
 import { StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Provider } from 'react-redux'
 import store from './redux/store';
 import { NavigationContainer } from '@react-navigation/native';
 import TransactionScreen from './screens/transaction';
 import SummaryScreen from './screens/summary';
-
+import { Ionicons } from '@expo/vector-icons';
+import TransactionDetailScreen from './screens/transactionDetail';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 function MyTabs() {
 	return (
 		<Tab.Navigator>
-			<Tab.Screen name="Transactions" component={TransactionScreen} />
-			<Tab.Screen name="Summary" component={SummaryScreen} />
+			<Tab.Screen 
+				name="Transactions" 
+				component={TransactionStack} 
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons name="file-tray-full-outline" color={color} size={size} />
+					),
+    			}}
+			/>
+			<Tab.Screen 
+				name="Summary" 
+				component={SummaryScreen} 
+				options={{
+					tabBarLabel: 'Summary',
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons name="bar-chart-outline" color={color} size={size} />
+					),
+				}}
+			/>
 		</Tab.Navigator>
 	);
 }
+
+
+const TransactionStack = () => {
+	return (
+	  <Stack.Navigator>
+		<Stack.Screen name="Transaction" component={TransactionScreen} options={{ headerShown: false }} />
+		<Stack.Screen name="Detail" component={TransactionDetailScreen}  options={{ headerShown: false }}/>
+	  </Stack.Navigator>
+	);
+  };
 
 export default function App() {
 	return (
