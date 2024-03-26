967f98c125110216a43a685cc54779d6142410a5
chiree sherpa <tsherisherpa@gmail.com>
2024-03-26 15:14:52 -0400

    created app.js

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Provider } from 'react-redux'
+import store from './redux/store';
+import { NavigationContainer } from '@react-navigation/native';
+import TransactionScreen from './screens/transaction';
+import SummaryScreen from './screens/summary';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+	return (
+		<Tab.Navigator>
+			<Tab.Screen name="Transactions" component={TransactionScreen} />
+			<Tab.Screen name="Summary" component={SummaryScreen} />
+		</Tab.Navigator>
+	);
 }
 
+export default function App() {
+	return (
+		<Provider store={store}>
+			<NavigationContainer>
+				<MyTabs />
+			</NavigationContainer>
+		</Provider>
+	);
+}
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
