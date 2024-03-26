6beb88268e77898526f00fecf3af0676c26a1433
chiree sherpa <tsherisherpa@gmail.com>
2024-03-26 17:48:25 -0400

    changes style of summary screen

--- screens/summary.js
+++ screens/summary.js
@@ -1,5 +1,61 @@
+import { Text, View, StyleSheet } from 'react-native'
+import { useSelector } from 'react-redux';
+import { selectHighestSpending, selectLeastAmountTransaction, selectTotalAmount, selectTotalTransactions } from '../redux/transactionSlice';
+
 export default function SummaryScreen() {
+
+    const totalAmount = useSelector(selectTotalAmount)
+    const totalTransactions = useSelector(selectTotalTransactions)
+    const highestSpending = useSelector(selectHighestSpending)
+    const lowestSpending = useSelector(selectLeastAmountTransaction)
+
+
     return (
-      <Text>HI</Text>
+        <View style={styles.container}>
+            <View style={styles.item}>
+                <Text style={styles.title}>Transactions: </Text>
+                <Text>{totalTransactions}</Text>
+            </View>
+            <View style={styles.item}>
+                <Text style={styles.title}>Balance:</Text>
+                <Text>{totalAmount}</Text>
+            </View>
+            <View>
+                <Text style={styles.subTitle}>Highest Spending:</Text>
+                <View style={styles.item}>
+                    <Text>{highestSpending.title}</Text>
+                    <Text> ${highestSpending.amount}</Text>
+                </View>
+            </View>
+            <View>
+                <Text style={styles.subTitle}>Lowest Spending:</Text>
+                <View style={styles.item}>
+                    <Text>{lowestSpending.title}</Text>
+                    <Text> ${lowestSpending.amount}</Text>
+                </View>
+            </View>
+        </View>
     );
-  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    item: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+    },
+    title: {
+        fontWeight: "800"
+    },
+    subTitle: {
+        padding: 10, 
+        fontWeight: 'bold', 
+        color: "blue" 
+    }
+});
